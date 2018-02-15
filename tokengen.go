3aecaee33fb5ef763bda7a73dafd88d620eee612
Al DeLucca <aldelucca@glasshouse.company>
2018-02-15 15:52:42 -0500

    fix: Breaking interface on uuid

--- tokengen.go
+++ tokengen.go
@@ -12,8 +12,11 @@ type AuthorizeTokenGenDefault struct {
 
 // GenerateAuthorizeToken generates a base64-encoded UUID code
 func (a *AuthorizeTokenGenDefault) GenerateAuthorizeToken(data *AuthorizeData) (ret string, err error) {
-	token := uuid.NewV4().String()
-	return base64.RawURLEncoding.EncodeToString([]byte(token)), nil
+	token, err := uuid.NewV4()
+	if err != nil {
+		return "", err
+	}
+	return base64.RawURLEncoding.EncodeToString([]byte(token.String())), nil
 }
 
 // AccessTokenGenDefault is the default authorization token generator
@@ -22,12 +25,18 @@ type AccessTokenGenDefault struct {
 
 // GenerateAccessToken generates base64-encoded UUID access and refresh tokens
 func (a *AccessTokenGenDefault) GenerateAccessToken(data *AccessData, generaterefresh bool) (accesstoken string, refreshtoken string, err error) {
-	token := uuid.NewV4().String()
-	accesstoken = base64.RawURLEncoding.EncodeToString([]byte(token))
+	token, err := uuid.NewV4()
+	if err != nil {
+		return "", "", err
+	}
+	accesstoken = base64.RawURLEncoding.EncodeToString([]byte(token.String()))
 
 	if generaterefresh {
-		rtoken := uuid.NewV4().String()
-		refreshtoken = base64.RawURLEncoding.EncodeToString([]byte(rtoken))
+		rtoken, err := uuid.NewV4()
+		if err != nil {
+			return "", "", err
+		}
+		refreshtoken = base64.RawURLEncoding.EncodeToString([]byte(rtoken.String()))
 	}
 	return
 }
